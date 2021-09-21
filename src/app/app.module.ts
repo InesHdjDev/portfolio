18ff702742fdc54acd0765f922cd87773d8a0294
Ines Hadjadji <ms.hadjadji.i@gmail.com>
2021-09-21 07:23:50 +0100

    generate about me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
