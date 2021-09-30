1594f1b1114d1ae5a8845086c3c7e6560008bf5c
Ines Hadjadji <ms.hadjadji.i@gmail.com>
2021-09-30 06:50:06 +0100

    about me section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener, Inject, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private el: ElementRef
+    ) { }
 
-  ngOnInit(): void {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      this.el.nativeElement.querySelector("#about-me").classList.add('fadeInUp');
+    }
   }
 
 }
