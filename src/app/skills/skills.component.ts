27e541d52daddf3b64db59dc0395d70a15977176
Ines Hadjadji <ms.hadjadji.i@gmail.com>
2021-10-20 06:56:08 +0100

    skills section animation

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private el: ElementRef
+    ) { }
 
-  ngOnInit(): void {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      this.el.nativeElement.querySelector("#skills-section").classList.add('fadeInUp');
+    }
   }
-
 }
