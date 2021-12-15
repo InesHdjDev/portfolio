491f2f2617094113f4c4230662a8bfa776ec9714
Ines Hadjadji <ms.hadjadji.i@gmail.com>
2021-12-15 06:23:55 +0100

    home direction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../services/global.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public global: GlobalService
+  ) { }
 
   ngOnInit(): void {
   }
